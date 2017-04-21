10adaf26c53a49468c744293325979947797aa5c
MentatKhan <penguinking1991@gmail.com>
2017-04-21 13:21:45 -0600

    removed old

--- prog33/prog33.hs
+++
@@ -1,7 +0,0 @@
-import Data.List
-
-
-fracs = [(a ,b) | a <-[1..9], b <-[1..9] , c <- [1..9], a*b*9+b*c == a*c*10, a `div` b < 1]
-
-problem_33 = foldr1 tupadd fracs where
-		tupadd (a,b) (c,d) = ( a*c `div` gcd( a*c)(b *d) , b*d `div` gcd (a*c) (b*d) )
